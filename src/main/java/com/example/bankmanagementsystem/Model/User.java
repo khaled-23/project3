15e39ddb4d2ca43974ac797b1589ccd6ac757016
khaled alkuhaily <khaledalkuhaily@gmail.com>
2024-04-24 09:17:15 +0300

    edited user

--- src/main/java/com/example/bankmanagementsystem/Model/User.java
+++ src/main/java/com/example/bankmanagementsystem/Model/User.java
@@ -10,9 +10,11 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
+import java.util.Collections;
 
 @Entity
 @Getter
@@ -53,7 +55,7 @@ public class User implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        return Collections.singleton(new SimpleGrantedAuthority(this.role));
     }
 
     @Override
